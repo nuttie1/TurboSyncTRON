36ea799cb7e7d33c9b9aa5bb60540fbde739d8e7
nikoalaa <Niko.Ala-aho@metropolia.fi>
2023-09-06 11:46:59 +0300

    Read user inputs to variables

--- src/main/java/com/example/otp1r4/LoginController.java
+++ src/main/java/com/example/otp1r4/LoginController.java
@@ -1,14 +1,25 @@
 package com.example.otp1r4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+
+    String username;
+    String password;
+
+    public void clickLogin(ActionEvent actionEvent) {
+        if(!usernameField.getText().isEmpty())
+            username = usernameField.getText();
+
+        if(!passwordField.getText().isEmpty())
+            password = passwordField.getText();
     }
+
 }
