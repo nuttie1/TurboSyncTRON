c0c776bbecd3cc2f2065ab5ab4faedcfb8e4f005
nuttie1 <otto.keksi@gmail.com>
2023-09-07 22:51:03 +0300

    Add: user insrting functions

--- src/main/java/com/example/otp1r4/dao/SignDAO.java
+++ src/main/java/com/example/otp1r4/dao/SignDAO.java
@@ -37,4 +37,49 @@ public class SignDAO implements DAO {
 
         throw new Exception("DB authentication went wrong");
     }
+
+    /** Add new user to database w/o security questions
+     *
+     * @param name
+     * @param password
+     */
+    public void addUser(String name, String password){
+        try {
+            String sql = "INSERT INTO `users`(`Name`, `Password`) VALUES (?,?)";
+            prepStat = conn.prepareStatement(sql);
+            prepStat.setString(1,name);
+            prepStat.setString(2,password);
+
+            prepStat.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Add new User to database with security questions
+     *
+     * @param name
+     * @param password
+     * @param securityQuestion1
+     * @param securityQuestion2
+     * @param securityQuestion3
+     */
+    public void addUser(String name, String password, String securityQuestion1, String securityQuestion2, String securityQuestion3){
+        try {
+            String sql = "INSERT INTO `users`(`Name`, `Password`, `Security1`, `Security2`, `Security3`) VALUES (?,?,?,?,?)";
+            prepStat = conn.prepareStatement(sql);
+            prepStat.setString(1,name);
+            prepStat.setString(2,password);
+            prepStat.setString(3,securityQuestion1);
+            prepStat.setString(4,securityQuestion2);
+            prepStat.setString(5,securityQuestion3);
+
+            prepStat.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
