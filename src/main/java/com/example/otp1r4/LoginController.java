601f35db9fc414665246355bfe2fab008baeeb8d
nikoalaa <Niko.Ala-aho@metropolia.fi>
2023-09-07 12:42:53 +0300

    Add error messages/placeholders to inputs
    Change password Texfield to Passwordfield

--- src/main/java/com/example/otp1r4/LoginController.java
+++ src/main/java/com/example/otp1r4/LoginController.java
@@ -2,6 +2,7 @@ package com.example.otp1r4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -10,23 +11,36 @@ public class LoginController {
     private TextField usernameField;
     @FXML
     private TextField passwordField;
+    @FXML
+    private Label errorLabelUsername;
+    @FXML
+    private Label errorLabelPassword;
 
     String username;
     String password;
 
     public void clickLogin(ActionEvent actionEvent) {
 
-        if(!usernameField.getText().matches("([A-Za-z0-9\\-\\_]+)")
-            || usernameField.getText().isEmpty()){
-            //when it not matches the pattern (1.0 - 6.0)
-            //set the textField empty
+        if(usernameField.getText().isEmpty()){
             usernameField.setText("");
-        } else {
+            errorLabelUsername.setText("Syötä käyttäjätunnus!");
+        } else if (!usernameField.getText().matches("([A-Za-z0-9\\-\\_]+)")){
+            errorLabelUsername.setText("Syötä käyttäjätunnus hyväksytyssä muodossa!");
+        } else if (true){
+            // Löytyykö kannasta
+        }
+        else {
             username = usernameField.getText();
         }
 
-        if(!passwordField.getText().isEmpty())
+        if(passwordField.getText().isEmpty()){
+            errorLabelPassword.setText("Syötä salasana!");
+        } else if (true) {
+            // Löytyykö kannasta
+        }
+        else {
             password = passwordField.getText();
+        }
     }
 
 }
