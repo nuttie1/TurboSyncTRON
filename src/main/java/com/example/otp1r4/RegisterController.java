b2fbf4472d10b5919701ad79864581ffc1c96534
Onsbohh <onni.pajula@metropolia.fi>
2023-09-08 18:01:22 +0300

    Kirjautuminen aukeaa cancel napista

--- src/main/java/com/example/otp1r4/RegisterController.java
+++ src/main/java/com/example/otp1r4/RegisterController.java
@@ -2,7 +2,9 @@ package com.example.otp1r4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -10,6 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class RegisterController {
@@ -56,11 +59,16 @@ public class RegisterController {
     private void initialize() {
 
     }
-    public void backButtonOnAction(ActionEvent event) {
+    public void backButtonOnAction(ActionEvent event) throws IOException {
         Stage stage = (Stage) backButton.getScene().getWindow();
         stage.close();
 
-        //AVAA LOGIN
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void submitButtonOnAction(ActionEvent event) {
