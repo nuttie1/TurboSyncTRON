d910b4c679dcf0049c852d1c5cb35d06dfa222cc
Onsbohh <onni.pajula@metropolia.fi>
2023-10-01 19:07:13 +0300

    Working test for registering.

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import com.example.otp1r4.Main;
 import com.example.otp1r4.controller.RegisterController;
+import com.example.otp1r4.dao.JDBCConnection;
 import com.example.otp1r4.dao.UserDAO;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,27 +15,32 @@ import org.controlsfx.tools.Platform;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RegisterTest {
-    private RegisterController controller;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
 
+public class RegisterTest extends ApplicationTest {
     UserDAO userDAO = new UserDAO();
 
-    @BeforeEach
-    public void setUp() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/java/com/example/otp1r4/controller/registerView.fxml"));
+    @Override
+    public void start(Stage stage) throws IOException {
+        JDBCConnection jdbcConnection = new JDBCConnection();
+        jdbcConnection.start();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/otp1r4/registerView.fxml"));
         Parent root = loader.load();
-        Stage stage = new Stage();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        controller = loader.getController();
+        stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @BeforeEach
+    public void setUp() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
     }
 
     @AfterAll
@@ -45,20 +53,16 @@ public class RegisterTest {
 
     @Test
     public void newUserTest() {
-        controller.usernameField.setText("TestUser");
-        controller.passwordField.setText("Password");
-        controller.questionOneField.setText("Q1");
-        controller.answerOneField.setText("A1");
-        controller.questionTwoField.setText("Q2");
-        controller.answerTwoField.setText("A2");
-        controller.questionThreeField.setText("Q3");
-        controller.answerThreeField.setText("A3");
+        clickOn("#usernameField").write("TestUser");
+        clickOn("#passwordField").write("Password");
+        clickOn("#questionOneField").write("Q1");
+        clickOn("#answerOneField").write("A1");
+        clickOn("#questionTwoField").write("Q2");
+        clickOn("#answerTwoField").write("A2");
+        clickOn("#questionThreeField").write("Q3");
+        clickOn("#answerThreeField").write("A3");
 
-        try {
-            controller.submitButtonOnAction(new ActionEvent());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        clickOn("#submitButton");
 
         assertTrue(userDAO.checkUser("TestUser"));
     }
