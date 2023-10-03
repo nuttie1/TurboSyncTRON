c751c58b0a37682708f8f89963c2fed1db36c12c
nuttie1 <otto.keksi@gmail.com>
2023-10-03 10:07:28 +0300

    Fix queries and statements

--- src/main/java/com/example/otp1r4/model/Device.java
+++ src/main/java/com/example/otp1r4/model/Device.java
@@ -3,14 +3,21 @@ package com.example.otp1r4.model;
 public class Device implements Model{
     String deviceId;
     String deviceName;
-    String deviceType;
+    String deviceDesc;
+    boolean deviceFavorite;
+    String deviceControl;
+    String format;
+    String unit;
 
-    public Device (String id, String name, String type) {
-        deviceId = id;
-        deviceName = name;
-        deviceType = type;
+    public Device(String deviceId, String deviceName, String deviceDesc, boolean deviceFavorite, String deviceControl, String format, String unit) {
+        this.deviceId = deviceId;
+        this.deviceName = deviceName;
+        this.deviceDesc = deviceDesc;
+        this.deviceFavorite = deviceFavorite;
+        this.deviceControl = deviceControl;
+        this.format = format;
+        this.unit = unit;
     }
-
     public String getDeviceName() {
         return deviceName;
     }
@@ -19,7 +26,23 @@ public class Device implements Model{
         return deviceId;
     }
 
-    public String getDeviceType() {
-        return deviceType;
+    public String getDeviceDesc() {
+        return deviceDesc;
+    }
+
+    public boolean isDeviceFavorite() {
+        return deviceFavorite;
+    }
+
+    public String getDeviceControl() {
+        return deviceControl;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 }
