0069937b29701c227f8f73ac8e0d3f58788033af
nuttie1 <otto.keksi@gmail.com>
2023-10-08 20:14:01 +0300

    Trying to get the tests working

---
+++ src/test/java/tests/SimpleTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void testAlwaysPasses() {
+        // Your test logic here
+        assertTrue(true); // This assertion always passes
+    }
+}
